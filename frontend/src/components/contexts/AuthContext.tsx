21d45b1c9b4188e81c949b7ef12482bc9a7d1c47
chimegerdene <chimgeebatuh@gmail.com>
2025-05-14 12:07:50 +0800

    auth context started

---
+++ frontend/src/components/contexts/AuthContext.tsx
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+
+// interface User {
+//   email: string;
+//   username: string;
+//   password: string;
+// }
+
+// interface AuthContextType {
+//   user: User;
+//   setUser: React.Dispatch<React.SetStateAction<User>>;
+// }
+
+export const AuthContext = createContext({
+  user: {
+    email: "",
+    username: "",
+    password: "",
+  },
+  setUser: () => {}, // placeholder
+});
